aab6b97b22817ec63ad72e45dc0bccc45607de4c
Gabriel Santos <gabriels.santosx@gmail.com>
2026-07-02 23:48:03 -0300

    fix(motivation): texto com tamanho adequado para mobile

--- src/components/motivation/subtitle-motivation.tsx
+++ src/components/motivation/subtitle-motivation.tsx
@@ -1,17 +1,16 @@
 export default function SubTitleMotivation() {
   return (
-    <div className="text-white font-inter leading-3 text-[12px] mt-25 text-center">
+    <div className="text-white font-inter leading-3 text-[12px] mt-27 text-center px-6">
       <p>
-        Vimos de perto marcas sendo tratadas sem estratégia, sem cuidado e sem{' '}
-        <br />
-        direção. Projetos atrasados, comunicação falha e <br /> decisões tomadas
-        sem clareza. Não fazia sentido continuar assim.
+        Vimos de perto marcas sendo tratadas sem estratégia, sem cuidado e sem
+        direção. Projetos atrasados, comunicação falha e decisões tomadas sem
+        clareza. Não fazia sentido continuar assim.
       </p>
-      <p className="mt-1">
+      <p className="mt-2">
         Criamos o Studio Claraval para ser o oposto disso.{' '}
         <span className="font-bold">
-          Um estúdio <br /> que pensa antes de executar, organiza antes de criar
-          e constrói <br /> marcas com intenção.
+          Um estúdio que pensa antes de executar, organiza antes de criar e
+          constrói marcas com intenção.
         </span>
       </p>
     </div>
